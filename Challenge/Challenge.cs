7efd38a7742d760004e81d78db8e9a3c0096ef68
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-11-10 17:58:42 +0100

    Add project files.

---
+++ Challenge/Challenge.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenge
+{
+    public class Challenge
+    {
+        public int NumberOfVowels(string sentence)
+        {
+            var listOfVowels = new List<string>
+        {
+            "a", "e", "i", "o", "u"
+        };
+            var numberOfVowels = sentence.Count(letter => listOfVowels.Contains(letter.ToString().ToLower()));
+            return numberOfVowels;
+        }
+        
+    }
+}
